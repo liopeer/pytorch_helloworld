60506c3c4b4c105153ce4a17b041392ff3a17f54
Lionel Peer <lionel.peer@gmail.com>
2023-09-20 15:57:42 +0200

    added submission script

--- subscript_trainparallel.sh
+++ subscript_trainparallel.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#SBATCH  --output=log/%j.out
+#SBATCH  --gres=gpu:2
+#SBATCH  --mem=10G
+#SBATCH  --job-name=double
+#SBATCH  --constraint='geforce_gtx_titan_x|geforce_rtx_2080_ti|titan_xp|geforce_gtx_1080_ti|titan_x|a100'
+
 source /scratch_net/biwidl311/peerli/conda/etc/profile.d/conda.sh
 conda activate liotorch
 mkdir log
